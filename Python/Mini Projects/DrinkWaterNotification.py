dcac42bac801403fda9ffa7ca3e1176f7b4fe22c
omkudale9198 <68637380+omkudale9198@users.noreply.github.com>
2021-11-15 12:20:50 +0530

    Add files via upload

---
+++ Python/Mini Projects/DrinkWaterNotification.py
@@ -0,0 +1,33 @@
+import datetime
+import time
+from win10toast import ToastNotifier
+
+
+def getTimeInput():
+    hours=int(input("Hours of interval :"))
+    minutes=int(input("Mins of interval :"))
+    seconds=int(input("Secs of interval :"))
+    time_interval=seconds+(minutes*60)+(hours*3600)
+    return  time_interval
+
+def log():
+    now=datetime.datetime.now()
+    start_time=now.strftime("%H:%M:%S")
+    with open("log.txt",'a') as f:
+        f.write(f"You drank water {now} \n")
+    return 0
+
+def notify():
+    notification=ToastNotifier()
+    notification.show_toast("Time to drink water")
+    log()
+    return 0
+
+def starttime(time_interval):
+    while True:
+        time.sleep(time_interval)
+        notify()
+
+if __name__=='__main__':
+    time_interval=getTimeInput()
+    starttime(time_interval)
